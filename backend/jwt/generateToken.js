79317924a334d86461d9c059dd25bf4ee289b3e7
Vikas <kuvikasar112@gmail.com>
2025-03-28 03:58:02 +0530

    domain error fixed

--- backend/jwt/generateToken.js
+++ backend/jwt/generateToken.js
@@ -9,9 +9,5 @@ export const generateToken = async (res, userId) => {
     secure: process.env.NODE_ENV !== "development",
     sameSite: "strict", // protect against csrf
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
-    domain:
-      process.env.NODE_ENV === "production"
-        ? "https://chatapp-5y51.onrender.com"
-        : "localhost",
   });
 };
